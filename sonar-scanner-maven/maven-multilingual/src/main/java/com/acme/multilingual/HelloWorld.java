c0dc9c830ad1aa6ae030946ca3171c097f70e828
admin <yj.seong@curvc.com>
2025-03-17 14:24:33 +0900

    test

--- sonar-scanner-maven/maven-multilingual/src/main/java/com/acme/multilingual/HelloWorld.java
+++ sonar-scanner-maven/maven-multilingual/src/main/java/com/acme/multilingual/HelloWorld.java
@@ -4,10 +4,11 @@ public class HelloWorld {
 
   void sayHello() {
     System.out.println("Hello World!");
-  }
+    System.out.println("Welcome to the multilingual application!");
+    }
 
-  void notCovered() {
-    System.out.println("This method is not covered by unit tests");
+    void notCovered() {
+    System.out.println("This method is not covered by unit tests.");
   }
 
 }
