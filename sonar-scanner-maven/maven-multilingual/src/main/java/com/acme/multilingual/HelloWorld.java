bdc847fdc579919264215b24d121390a60de03ca
admin <yj.seong@curvc.com>
2025-03-19 11:26:47 +0900

    test

--- sonar-scanner-maven/maven-multilingual/src/main/java/com/acme/multilingual/HelloWorld.java
+++ sonar-scanner-maven/maven-multilingual/src/main/java/com/acme/multilingual/HelloWorld.java
@@ -1,19 +1,7 @@
 package com.acme.multilingual;
 
 public class HelloWorld {
-  private String language;
 
-  public HelloWorld(String language) {
-    this.language = language;
-  }
-
-  public String getLanguage() {
-    return language;
-  }
-
-  public void setLanguage(String language) {
-    this.language = language;
-  }
   void sayHello() {
     System.out.println("Hello World!");
     System.out.println("Welcome to the multilingual application!");
