b3594612d1632904f53fcef804d5355d864e8fa4
admin <yj.seong@curvc.com>
2025-03-19 11:29:30 +0900

    tets

--- sonar-scanner-maven/maven-multilingual/src/main/java/com/acme/multilingual/HelloWorld.java
+++ sonar-scanner-maven/maven-multilingual/src/main/java/com/acme/multilingual/HelloWorld.java
@@ -5,11 +5,12 @@ public class HelloWorld {
   void sayHello() {
     System.out.println("Hello World!");
     System.out.println("Welcome to the multilingual application!");
-      public static void main(String[] args) {
-        HelloWorld helloWorld = new HelloWorld();
-        helloWorld.sayHello();
-      }
-    }
+  }
+
+  public static void main(String[] args) {
+    HelloWorld helloWorld = new HelloWorld();
+    helloWorld.sayHello();
+  }
 
     void notCovered() {
     System.out.println("This method is not covered by unit tests.");
