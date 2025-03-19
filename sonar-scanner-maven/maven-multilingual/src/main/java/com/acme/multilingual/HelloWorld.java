e122617f28a40b65a6cc1dde1f9ca4983d575c9b
admin <yj.seong@curvc.com>
2025-03-19 11:28:22 +0900

    test

--- sonar-scanner-maven/maven-multilingual/src/main/java/com/acme/multilingual/HelloWorld.java
+++ sonar-scanner-maven/maven-multilingual/src/main/java/com/acme/multilingual/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
   void sayHello() {
     System.out.println("Hello World!");
     System.out.println("Welcome to the multilingual application!");
+      public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.sayHello();
+      }
     }
 
     void notCovered() {
